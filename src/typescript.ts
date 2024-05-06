54b6744c93b6f9b8244e9283026b1775b80c956b
Luiz Miguel <barroslopesluizmiguel@gmail.com>
2024-05-06 10:59:27 -0300

    adicionado array e enums no arquivo TS

--- src/typescript.ts
+++ src/typescript.ts
@@ -1,3 +1,39 @@
+
+// tipos primitivos em TS
 let valor: number = 3000;
 let nome: string = "";
 let foiPago: boolean = false;
+let qualquer: any = "";
+qualquer = 22;
+qualquer = "Qualquer coisa";
+
+// Arrays
+
+const Lista: number[] = []
+
+Lista.push(3, 4, 22, 5)
+
+//Tipos personalizados TYPE, por isso o nome TYPEscript. Pela declaração de "typos" de variáveis 
+
+type Transacao = {
+    tipoTransacao: TipoTransacao;
+    data: Date;
+    valor: number
+
+}
+
+
+enum TipoTransacao {
+    DEPOSITO = "Depósito",
+    TRANSFERENCIA = "Transferência",
+    PAGAMENTO_BOLETO = "Pagamento de Boleto"
+}
+
+const novaTransacao: Transacao = {
+    tipoTransacao: TipoTransacao.PAGAMENTO_BOLETO,
+    data: new Date(),
+    valor: 0
+
+
+}
+
