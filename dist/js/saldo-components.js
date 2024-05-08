bb01d839c73bc21e901f884962ed0d9d889de222
Luiz Miguel <barroslopesluizmiguel@gmail.com>
2024-05-08 08:28:44 -0300

    data formatada

--- dist/js/saldo-components.js
+++ dist/js/saldo-components.js
@@ -1,6 +1,16 @@
 // e aqui vai ficar onde a parte de gerenciamento de saldo deve ficar
 let saldo = 3000;
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoSaldo != null) {
     elementoSaldo.textContent = saldo.toLocaleString("pt-br", { currency: "BRL", style: "currency" });
+}
+if (elementoDataAcesso != null) {
+    const dataAcesso = new Date();
+    elementoDataAcesso.textContent = dataAcesso.toLocaleDateString("pt-br", {
+        weekday: "long",
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric"
+    });
 }
